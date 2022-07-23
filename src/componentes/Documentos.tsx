1f356f0bac26f2c99d6aa33ca3c07f1dead0d9e4
Litriboxer <litriboxer@hotmail.com>
2022-07-23 20:18:21 +0200

    pruebas de pdef en modal

--- src/componentes/Documentos.tsx
+++ src/componentes/Documentos.tsx
@@ -1,15 +1,47 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import '../css/styles.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-class Documentos extends React.Component {
 
+
+
+class Documentos extends React.Component {
+    
 
 render(): React.ReactNode {
     return (
         <div>
 <h1>Componente documentos</h1>
+
+
+
+<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModalPDF">
+  Launch demo modal
+</button>
+
+
+<div className="modal fade"  id="ModalPDF"  aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog" role="document">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Curriculum</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+      <object className="PDFdoc" width="100%" height="500px" type="application/pdf" data="../Pdf/Curriculum_Full_Stack.pdf"></object>
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-info">Descargar <i className="far fa-file-pdf ml-1 text-white"></i></button>
+        <button type="button" className="btn btn-outline-info waves-effect" data-dismiss="modal">Cerrar <i className="fas fa-times ml-1"></i></button>
+        
+        
+      </div>
+    </div>
+  </div>
+</div>
+
         </div>
           
            );
@@ -17,3 +49,7 @@ render(): React.ReactNode {
      }
      
      export default Documentos;
+
+function setShow(arg0: boolean) {
+    throw new Error('Function not implemented.');
+}
