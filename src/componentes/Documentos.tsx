ab9f73cbeb785c4ddec4106d6f574bacac6750fc
Litriboxer <litriboxer@hotmail.com>
2022-07-24 20:29:32 +0200

    Visor y descarga de pdf terminado. Creo botón para video Curriculum

--- src/componentes/Documentos.tsx
+++ src/componentes/Documentos.tsx
@@ -3,12 +3,14 @@ import '../css/styles.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CurriculumPdf from './CurriculumPdf';
 import { Button, Modal } from 'react-bootstrap';
-import ImagenBoton from "../multimedia/pdf.png";
+import ImagenBotonPdf from "../multimedia/pdf.png";
+import ImagenBotonVideo from "../multimedia/video.png";
+
 
 
 
 class Documentos extends React.Component {
-   show: boolean;
+  show: boolean;
 
   constructor(props: any) {
     super(props);
@@ -23,45 +25,53 @@ class Documentos extends React.Component {
   handleShow = () => {
     this.show = true;
 
+
     this.setState({ show: true });
-  }; 
+  };
 
 
   render(): React.ReactNode {
     return (
       <div className="bg-gradient row container-xxl-1">
-       
-        <div className="col-2 ms-5">
-        <h3 className="h1">Curriculum Pdf</h3>
+
+        <div className="row container-xxl-1 align-items-center justify-content-center">
+
+        <div className="col-5 ms-5">
+          <h3 className="h1 text-secondary">Curr&iacute;culum Pdf</h3>
         </div>
         <div className="col-2">
-        <Button
-          onClick={this.handleShow}
-          variant="secondary"
-          className="btn btn-outline-secondary ms-5"
-          id="botonNuevo"
-        >
-          <img src={ImagenBoton}
-            alt="boton abrir pdf" width={70} height={60} />
-        </Button>
+          <Button
+            onClick={this.handleShow}
+            variant="primary"
+            className=""
+            id="botonPdf"
+          >
+            <img src={ImagenBotonPdf}
+              alt="boton abrir pdf" width={70} height={70} />
+          </Button>
 
-        <Modal  className="modalWidth" show={this.show} onHide={this.handleClose} animation={true}>
-          <Modal.Header closeButton>
-            <Modal.Title>Curriculum PDF</Modal.Title>
-          </Modal.Header>
-          <Modal.Body >
-            <CurriculumPdf />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button>
-              descargar
-            </Button>
-
-          </Modal.Footer>
-        </Modal> 
+          <Modal size="lg" show={this.show} onHide={this.handleClose} animation={true}>
+            <Modal.Header closeButton className='bg-gradient'>
+              <Modal.Title><h3 className="h1 ms-5">Curr&iacute;culum PDF</h3></Modal.Title>
+            </Modal.Header>
+            <Modal.Body className='bg-gradient'>
+              <CurriculumPdf />
+            </Modal.Body>
+          </Modal>
+        </div>
         </div>
 
-        
+        <div className="row mt-5 container-xxl-1 align-items-center justify-content-center">
+        <div className="col-5 ms-5">
+          <h3 className="h1 text-secondary">Video Curr&iacute;culum</h3>
+        </div>
+        <div className="col-2">
+         <Button> <img src={ImagenBotonVideo}
+              alt="boton abrir video" width={70} height={70} /></Button>
+        </div>
+
+        </div>
+
 
       </div>
 
