139d807cfe1d5adb0c6ab1a901fee674efce621d
Litriboxer <litriboxer@hotmail.com>
2022-07-25 09:06:57 +0200

    creo modales con documentos pero siempre muestra mismo modal

--- src/componentes/DiplomaPdf.tsx
+++ src/componentes/DiplomaPdf.tsx
@@ -6,10 +6,10 @@ import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import { Worker } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import Curriculum from '../pdf/Curriculum.pdf';
+import Diploma from '../pdf/Diploma_Full_Stack.pdf';
 
-export const CurriculumPdf = () =>  {
-    const [defaultCurriculum]=useState(Curriculum);
+export const DiplomaPdf = () =>  {
+    const [defaultDiploma]=useState(Diploma);
  
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
@@ -18,8 +18,8 @@ export const CurriculumPdf = () =>  {
         <div>
          
          {/* show pdf conditionally (if we have one)  */}
-        {defaultCurriculum&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-          <Viewer fileUrl={Curriculum}
+        {defaultDiploma&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+          <Viewer fileUrl={Diploma}
           plugins={[defaultLayoutPluginInstance]}/>
             
             </Worker></>}
@@ -32,4 +32,4 @@ export const CurriculumPdf = () =>  {
     }
   
   
-  export default CurriculumPdf;
+  export default DiplomaPdf;
--- src/componentes/Documentos.tsx
+++ src/componentes/Documentos.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../css/styles.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CurriculumPdf from './CurriculumPdf';
 import { Button, Modal } from 'react-bootstrap';
 import ImagenBotonPdf from "../multimedia/pdf.png";
 import ImagenBotonVideo from "../multimedia/video.png";
+import DiplomaPdf from './DiplomaPdf';
+import CurriculumPdf from './CurriculumPdf';
 
 
 
@@ -15,17 +16,15 @@ class Documentos extends React.Component {
   constructor(props: any) {
     super(props);
     this.show = false;
-
   }
 
   handleClose = () => {
     this.show = false;
     this.setState({ show: false });
   };
+
   handleShow = () => {
     this.show = true;
-
-
     this.setState({ show: true });
   };
 
@@ -36,42 +35,85 @@ class Documentos extends React.Component {
 
         <div className="row container-xxl-1 align-items-center justify-content-center">
 
-        <div className="col-5 ms-5">
-          <h3 className="h1 text-secondary">Curr&iacute;culum Pdf</h3>
-        </div>
-        <div className="col-2">
-          <Button
-            onClick={this.handleShow}
-            variant="primary"
-            className=""
-            id="botonPdf"
-          >
-            <img src={ImagenBotonPdf}
-              alt="boton abrir pdf" width={70} height={70} />
-          </Button>
+          <div className="col-5 ms-5">
+            <h3 className="h1 text-secondary">Curr&iacute;culum Pdf</h3>
+          </div>
+          <div className="col-2">
+            <Button
+              onClick={this.handleShow}
+              variant="primary"
+              id="botonModalCurr&iacute;culum"
+            >
+              <img src={ImagenBotonPdf}
+                alt="boton abrir pdf" width={70} height={70} />
+            </Button>
 
-          <Modal size="lg" show={this.show} onHide={this.handleClose} animation={true}>
-            <Modal.Header closeButton className='bg-gradient'>
-              <Modal.Title><h3 className="h1 ms-5">Curr&iacute;culum PDF</h3></Modal.Title>
-            </Modal.Header>
-            <Modal.Body className='bg-gradient'>
-              <CurriculumPdf />
-            </Modal.Body>
-          </Modal>
-        </div>
+            <Modal id="ModalCurr&iacute;culum" size="lg" show={this.show} onHide={this.handleClose} animation={true}>
+              <Modal.Header closeButton className='bg-gradient'>
+                <Modal.Title><h3 className="h1 ms-5">Curr&iacute;culum PDF</h3></Modal.Title>
+              </Modal.Header>
+              <Modal.Body className='bg-gradient'>
+                <CurriculumPdf />
+              </Modal.Body>
+            </Modal>
+          </div>
         </div>
 
         <div className="row mt-5 container-xxl-1 align-items-center justify-content-center">
-        <div className="col-5 ms-5">
-          <h3 className="h1 text-secondary">Video Curr&iacute;culum</h3>
-        </div>
-        <div className="col-2">
-         <Button> <img src={ImagenBotonVideo}
-              alt="boton abrir video" width={70} height={70} /></Button>
+          <div className="col-5 ms-5">
+            <h3 className="h1 text-secondary">Video Curr&iacute;culum</h3>
+          </div>
+          <div className="col-2">
+            <Button
+              onClick={this.handleShow}
+              variant="primary"
+              id="botonModalVideoCurr&iacute;culum"
+            >
+              <img src={ImagenBotonVideo}
+                alt="boton abrir video" width={70} height={70} />
+            </Button>
+
+            <Modal id="ModalVideoCurr&iacute;culum" size="lg" show={this.show} onHide={this.handleClose} animation={true}>
+              <Modal.Header closeButton className='bg-gradient'>
+                <Modal.Title><h3 className="h1 ms-5">Video Curr&iacute;culum</h3></Modal.Title>
+              </Modal.Header>
+              <Modal.Body className='bg-gradient'>
+                <iframe className='ms-5'
+                  width="560" height="315" src="https://www.youtube-nocookie.com/embed/lwZ2yGDT7vo" title="YouTube video player"
+                  frameBorder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen>
+                </iframe>
+              </Modal.Body>
+            </Modal>
+          </div>
         </div>
 
-        </div>
 
+        <div className="row mt-5 container-xxl-1 align-items-center justify-content-center">
+
+          <div className="col-5 ms-5">
+            <h3 className="h1 text-secondary">Diplomas Curso Full Stack Pdf</h3>
+          </div>
+          <div className="col-2">
+            <Button
+              onClick={this.handleShow}
+              variant="primary"
+              id="botonModalDiplomas"
+            >
+              <img src={ImagenBotonPdf}
+                alt="boton abrir pdf" width={70} height={70} />
+            </Button>
+
+            <Modal id="ModalDiplomas" size="lg" show={this.show} onHide={this.handleClose} animation={true}>
+              <Modal.Header closeButton className='bg-gradient'>
+                <Modal.Title><h3 className="h1 ms-5">Diplomas Curso Full Stack Pdf</h3></Modal.Title>
+              </Modal.Header>
+              <Modal.Body className='bg-gradient'>
+                <DiplomaPdf />
+              </Modal.Body>
+            </Modal>
+          </div>
+        </div>
 
       </div>
 
